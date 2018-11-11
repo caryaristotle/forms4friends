504192a054817a17f6b84aa2303f392298f7f5fc
Hossam Mabed <hossam.nasr99@gmail.com>
2018-11-10 20:12:12 -0500

    routing

---
+++ forms-app/src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+
+const MainMenu = () => (
+  <div>
+    <Link to="/">
+      <button>home</button>
+    </Link>
+    <Link to="/about">
+      <button>About</button>
+    </Link>
+    <Link to="/code">
+      <button>code</button>
+    </Link>
+    <Link to="/code">
+      <button>contact</button>
+    </Link>
+    <Link to="/info">
+      <button>info</button>
+    </Link>
+  </div>
+);
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+);
+
+const About = () => <div>About</div>;
+
+const Code = () => <div>Code</div>;
+
+const Contact = () => <div>Contact</div>;
+
+const info = () => <div>info</div>;
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/code" component={Code} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/presence" component={info} />
+          </div>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <MainMenu />
+          </header>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
+import { Container, Button } from "./styles";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import Button from './Components/button/initialform';
 class LandingPage extends Component {
-  render () {
-    return <Button/>
+  render() {
+    return (
+      <Container>
+        <Link to="/forms">
+          <Button />
+        </Link>
+        <Link to="/tree">
+          <Button />
+        </Link>
+        <Link to="/create">
+          <Button />
+        </Link>
+      </Container>
+    );
   }
 }
 
