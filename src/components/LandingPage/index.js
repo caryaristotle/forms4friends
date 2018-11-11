127537f7cdebcde55eb410355d3afa7d2264e15d
[carissa] <[carissachen2017@gmail.com]>
2018-11-10 21:48:24 -0500

    added treepage style stuff

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -6,14 +6,15 @@ class LandingPage extends Component {
   render() {
     return (
         <Container>
+          <h1>Welcome to Legal Form Search!</h1>
           <Link to="/forms">
-            <Button />
+            <Button> I know what forms to fill</Button>
           </Link>
           <Link to="/tree">
-            <Button />
+            <Button> Help me find what forms to fill </Button>
           </Link>
           <Link to="/create">
-            <Button />
+          <Button> Create a new form. Trust me, I'm a lawyer. </Button>
           </Link>
         </Container>
     );
--- src/components/TreePage/index.js
+++ src/components/TreePage/index.js
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 
 class FormsPage extends Component {
   render() {
-    return <h1>Tree Page!</h1>;
+    return (
+      <Container>
+        <h1>Please select the form </h1>
+        
+        <Link to="/forms">
+          <Button> I know what forms to fill</Button>
+        </Link>
+        <Link to="/tree">
+          <Button> Help me find what forms to fill </Button>
+        </Link>
+        <Link to="/create">
+        <Button> Create a new form. Trust me, I'm a lawyer. </Button>
+        </Link>
+      </Container>
+  );
   }
 }
 
